9f1e667524887f7813f55185908bb0dea386c9aa
Qingyu Shang <2931013282@qq.com>
2022-09-26 17:28:36 +0800

    [script] add scripts for test paylaod or u-boot
    Signed-off-by: Qingyu Shang <2931013282@qq.com>

---
+++ build_fw_hello.sh
@@ -0,0 +1,9 @@
+cd Penglai-Enclave-sPMP/opensbi-0.9
+rm -r build
+make ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu- PLATFORM=generic FW_FDT_PATH=../../u-boot/u-boot.dtb
+rm -r ../../payload/penglai-1.0/fw_payload*
+cp build/platform/generic/firmware/fw_payload.bin ../../payload/penglai-1.0
+cd ../../payload/penglai-1.0
+ls -l
+../fsz.sh fw_payload.bin fw_payload.bin.out
+cd ../..
